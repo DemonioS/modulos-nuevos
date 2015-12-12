26230b54401a691e153066a2cf76281fe664b4b2
DemonioS <alejandrowenzel@gmail.com>
2015-12-12 15:01:55 -0300

    remuneracion v2

--- proyecto_inacap_indicadores/__openerp__.py
+++ proyecto_inacap_indicadores/__openerp__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
  
 {
-    'name' : 'indicadores financieros de chile',
+    'name' : 'remuneracion de chile',
     'version' : '1.0',
     'author' : 'Alejandro Wenzel',
     'summary' : 'proyecto titulo',
     'aplication' : True,    
-    'description' : 'indicadores chilenos para el calculo de remuneraciones ',
+    'description' : 'indicadores chilenos y remuneraciones ',
     'depends' : [
             'base',
             ],
@@ -14,7 +14,8 @@
             'indicadores1_view.xml', 
             'indicadores2_view.xml', 
             'indicadores3_view.xml', 
-                ],
+            'remuneraciones_view.xml', 
+              ],
     'installable' : True,
     'aplication' : True,    
 }
